c438074798f59a18d031e26810b2e63fc5c31d51
John N. Norman <normanj777@gmail.com>
2022-07-28 14:49:42 -0400

    add footer

--- src/Routes/SharedLayout.jsx
+++ src/Routes/SharedLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer";
 import NavBar from "../Components/NavBar";
 
 const SharedLayout = () => {
@@ -6,7 +7,7 @@ const SharedLayout = () => {
     <>
       <NavBar />
       <Outlet />
-      <h2>Footer</h2>
+      <Footer />
     </>
   );
 };
