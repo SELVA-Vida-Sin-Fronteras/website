f6fe2a84b7a07a25ea1acf9c623648ad340b6754
John N. Norman <normanj777@gmail.com>
2022-07-28 11:30:34 -0400

    add remaining routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SharedLayout from './Routes/SharedLayout';
-import Home from './Routes/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SharedLayout from "./Routes/SharedLayout";
+
+import Home from "./Routes/Home";
+import Blog from "./Routes/Blog";
+import EnrichmentJourney from "./Routes/EnrichmentJourney";
+import TakeAction from "./Routes/TakeAction";
+import WhoWeAre from "./Routes/WhoWeAre";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SharedLayout/>} >
-          <Route index element={<Home/>}/>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="WhoWeARe" element={<WhoWeAre />} />
+          <Route path="EnrichmentJourney" element={<EnrichmentJourney />} />
+          <Route path="Blog" element={<Blog />} />
+          <Route path="TakeAction" element={<TakeAction />} />
+          <Route path="*" element={<h1>401 file not found</h1>} />
         </Route>
-        
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Routes/EnrichmentJourney.jsx
@@ -0,0 +1,5 @@
+const EnrichmentJourney = () => {
+  return <h1>EnrichmentJourney Page</h1>;
+};
+
+export default EnrichmentJourney;
