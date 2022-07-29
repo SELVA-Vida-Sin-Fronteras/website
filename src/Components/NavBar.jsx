fc159a003f1f3cbd4b36042b487e2e8c3ec926e2
John N. Norman <normanj777@gmail.com>
2022-07-29 07:17:42 -0400

    add content

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,20 @@
 import { NavLink } from "react-router-dom";
+import { logo } from "../assets/logo.jpg";
 
 const NavBar = () => {
   return (
     <>
-      <h1>hello navbar</h1>
-      <NavLink to="/">Home</NavLink>{" | "}
-      <NavLink to="/WhoWeAre">Who We Are</NavLink>{" | "}
-      <NavLink to="/EnrichmentJourney">Enrichment Journey</NavLink>{" | "}
-      <NavLink to="/Blog">Blog</NavLink>{" | "}
-      <NavLink to="/TakeAction">Take Action</NavLink>{" | "}
+      <h1>Friends of SELVA Vida Sin Fronteras</h1>
+      <NavLink to="/">Home</NavLink>
+      {" | "}
+      <NavLink to="/WhoWeAre">Who We Are</NavLink>
+      {" | "}
+      <NavLink to="/EnrichmentJourney">Enrichment Journey</NavLink>
+      {" | "}
+      <NavLink to="/Blog">Blog</NavLink>
+      {" | "}
+      <NavLink to="/TakeAction">Take Action</NavLink>
+      {" | "}
     </>
   );
 };
