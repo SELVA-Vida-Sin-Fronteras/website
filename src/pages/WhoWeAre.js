3b993dcb223708337db9ab2cb8aad7413e2196c1
John N. Norman <normanj777@gmail.com>
2022-08-22 07:34:35 -0400

    add responsive design

--- src/pages/WhoWeAre.js
+++ src/pages/WhoWeAre.js
@@ -1,41 +1,43 @@
 const WhoWeAre = () => {
   return (
     <div data-testid="who-we-are" className="bg-light p-5">
-      <section className="card mb-4">
-        <h1 className="card-header fs-1 fw-bold">Who We Are</h1>
-        <p className="card-body">
-          We are Friends of SELVA Vida Sin Fronteras-U.S.A., a not-for-profit
-          formed on March 1, 2019 in the State of Texas and qualifies as
-          tax-exempt under Section 501 (c) (3) of the U.S. Internal Revenue
-          Code. Our organization is focused on assisting the indigenous peoples
-          of the Amazon Rainforest to have a viable and sustainable future. We
-          intend to create an awareness of the need for preserving and
-          protecting the Amazon Rainforest and it's indigenous communities
-          through education, educational expeditions, and other means of
-          support. We are partners with SELVA Vida Sin Fronteras in Ecuador and
-          their supporters in Holland to achieve our goals.
-        </p>
-      </section>
-      <section className="card mb-4">
-        <h2 className="card-header fs-1 fw-bold">Our Mission</h2>
-        <p className="card-body">
-          Our Mission is to protect the flora and fauna of the Amazon
-          Rainforest, its ancestral indigenous communities, and the Galapagos
-          Archipelago and Marine Reserve by increasing awareness for cultural
-          and environmental preservation through educational experiences.
-        </p>
-      </section>
-      <section className="card mb-4">
-        <h2 className="card-header fs-1 fw-bold">Our Vision</h2>
-        <p className="card-body">
-          Our Vision is to partner with indigenous communities and environmental
-          organizations to help preserve the Ecuadorian Amazon Rainforest by
-          providing educational expeditions, support related projects and an
-          exchange of culture and arts. (Our institutional philosophy is guided
-          by the principle that Nature and all forms of life have the
-          Inalienable Right to Exist.”)
-        </p>
-      </section>
+      <div className="container m-auto" style={{ maxWidth: "50rem" }}>
+        <section className="card mb-4">
+          <h1 className="card-header fs-1 fw-bold">Who We Are</h1>
+          <p className="card-body">
+            We are Friends of SELVA Vida Sin Fronteras-U.S.A., a not-for-profit
+            formed on March 1, 2019 in the State of Texas and qualifies as
+            tax-exempt under Section 501 (c) (3) of the U.S. Internal Revenue
+            Code. Our organization is focused on assisting the indigenous
+            peoples of the Amazon Rainforest to have a viable and sustainable
+            future. We intend to create an awareness of the need for preserving
+            and protecting the Amazon Rainforest and it's indigenous communities
+            through education, educational expeditions, and other means of
+            support. We are partners with SELVA Vida Sin Fronteras in Ecuador
+            and their supporters in Holland to achieve our goals.
+          </p>
+        </section>
+        <section className="card mb-4">
+          <h2 className="card-header fs-1 fw-bold">Our Mission</h2>
+          <p className="card-body">
+            Our Mission is to protect the flora and fauna of the Amazon
+            Rainforest, its ancestral indigenous communities, and the Galapagos
+            Archipelago and Marine Reserve by increasing awareness for cultural
+            and environmental preservation through educational experiences.
+          </p>
+        </section>
+        <section className="card mb-4">
+          <h2 className="card-header fs-1 fw-bold">Our Vision</h2>
+          <p className="card-body">
+            Our Vision is to partner with indigenous communities and
+            environmental organizations to help preserve the Ecuadorian Amazon
+            Rainforest by providing educational expeditions, support related
+            projects and an exchange of culture and arts. (Our institutional
+            philosophy is guided by the principle that Nature and all forms of
+            life have the Inalienable Right to Exist.”)
+          </p>
+        </section>
+      </div>
     </div>
   );
 };
