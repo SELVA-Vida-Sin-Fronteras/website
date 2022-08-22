48c7775822736b94799ce10c88004c69ef601e5e
John N. Norman <normanj777@gmail.com>
2022-08-22 07:53:33 -0400

    add responsive design

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,41 +3,44 @@ import river from "../assets/img/blog-img.jpg";
 
 const Blog = () => {
   return (
-    <div data-testid="blog" className="container">
+    <div data-testid="blog">
       <section className="text-center bg-light">
-        <h1 className="container display-1">Friends of SELVA Blog</h1>
+        <h1 className="display-1">Friends of SELVA Blog</h1>
       </section>
-
-      <section className="featured card p-4 p-md-5 mb-4 text-white rounded bg-dark">
-        <div className="row g-0">
-          <div className="col-md-8">
-            <div className="card-body">
-              <h2 className="card-title display-4 fst-italic">
-                Inspiration from the Rainforest
-              </h2>
-              <p className="card-text lead my-3">
-                It's easy to sit in a comfortable armchair dreaming of saving
-                the world or at least having some impact on the environment.
-                It's quite another thing to walk through ancient forests and
-                along the Amazon rainforest basin, experiencing natural forces
-                at work.
-              </p>
-              <p className="card-text lead mb-0">
-                <a href="#" className="text-white fw-bold">
-                  Continue reading...
-                </a>
-              </p>
+      <div className="container">
+        <section className="featured card p-4 p-md-5 mb-4 text-white rounded bg-dark">
+          <div className="row g-0">
+            <div className="col-lg-8">
+              <div className="card-body">
+                <h2 className="card-title display-4 fst-italic">
+                  Inspiration from the Rainforest
+                </h2>
+                <p className="card-text lead my-3">
+                  It's easy to sit in a comfortable armchair dreaming of saving
+                  the world or at least having some impact on the environment.
+                  It's quite another thing to walk through ancient forests and
+                  along the Amazon rainforest basin, experiencing natural forces
+                  at work.
+                </p>
+                <p className="card-text lead mb-0">
+                  <a href="#" className="text-white fw-bold">
+                    Continue reading...
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <img
+                className="img-fluid"
+                src={Tagua}
+                alt="Sculpture wood and woven fabric resembling seed pods hanging from a branch"
+              />
             </div>
           </div>
-          <div className="col-md-2">
-            <img
-            className="img-fluid"
-              src={Tagua}
-              alt="Sculpture wood and woven fabric resembling seed pods hanging from a branch"
-            />
-          </div>
-        </div>
-      </section>
+        </section>
+
+        <div className="row">
+          <div className="col-md-8">
             <section className="blog-posts p-4">
               <article className="text-dark fs-4">
                 <h2 className="display-6">
@@ -59,25 +62,41 @@ const Blog = () => {
                 </p>
               </article>
             </section>
-
-            <h2 className="display-2">Archives</h2>
-            <ul className="list-group">
-              <li className="list-group-item fs-3">
-                <a className="archive-link" href="#">November 15, 2021</a>
-              </li>
-              <li className="list-group-item fs-3">
-                <a className="archive-link" href="#">November 2, 2021</a>
-              </li>
-              <li className="list-group-item fs-3">
-                <a className="archive-link" href="#">October 27, 2021</a>
-              </li>
-              <li className="list-group-item fs-3">
-                <a className="archive-link" href="#">October 26, 2021</a>
-              </li>
-              <li className="list-group-item fs-3">
-                <a className="archive-link" href="#">May 5, 2021</a>
-              </li>
-            </ul>
+          </div>
+          <div className="col-md-4">
+            <section className="archives">
+              <h2 className="display-2">Archives</h2>
+              <ul className="list-group">
+                <li className="list-group-item fs-3">
+                  <a className="archive-link" href="#">
+                    November 15, 2021
+                  </a>
+                </li>
+                <li className="list-group-item fs-3">
+                  <a className="archive-link" href="#">
+                    November 2, 2021
+                  </a>
+                </li>
+                <li className="list-group-item fs-3">
+                  <a className="archive-link" href="#">
+                    October 27, 2021
+                  </a>
+                </li>
+                <li className="list-group-item fs-3">
+                  <a className="archive-link" href="#">
+                    October 26, 2021
+                  </a>
+                </li>
+                <li className="list-group-item fs-3">
+                  <a className="archive-link" href="#">
+                    May 5, 2021
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Enrichment.js
+++ src/pages/Enrichment.js
@@ -2,61 +2,63 @@ import slide1 from "../assets/img/clinic-1.jpg";
 import slide2 from "../assets/img/clinic-2.jpg";
 import slide3 from "../assets/img/dining-1.jpg";
 
-
 const Enrichment = () => {
   return (
     <div
       data-testid="enrichment"
       className="bg-light p-5 justify-content-center"
     >
-      <section className="card m-auto" >
-        <h1 className="card-title card-header">Enrichment Journeys</h1>
+      <div className="container">
+        <section className="card m-auto" style={{maxWidth: "40rem"}}>
+          <h1 className="card-title card-header">Enrichment Journeys</h1>
 
-        <p className="card-body">
-          While temporarily on hold due to Covid-19 we do plan to go again in
-          Spring of 2023.
-        </p>
-      </section>
-      <div
-        id="carouselExampleControls"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={slide1} className="d-block w-100" alt="..." />
+          <p className="card-body">
+            While temporarily on hold due to Covid-19 we do plan to go again in
+            Spring of 2023.
+          </p>
+        </section>
+        <div
+          id="carouselExampleControls"
+          className="carousel slide mt-5"
+          data-bs-ride="carousel"
+          aria-hidden="true"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-bs-interval="10000">
+              <img src={slide1} className="d-block w-100" alt="" />
+            </div>
+            <div className="carousel-item">
+              <img src={slide2} className="d-block w-100" alt="" />
+            </div>
+            <div className="carousel-item">
+              <img src={slide3} className="d-block w-100" alt="" />
+            </div>
           </div>
-          <div className="carousel-item">
-            <img src={slide2} className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src={slide3} className="d-block w-100" alt="..." />
-          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
     </div>
   );
