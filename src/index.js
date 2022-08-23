e818d6a7c79664fcb3c5b43d2c6f04f49fa44933
John N. Norman <normanj777@gmail.com>
2022-08-23 07:20:33 -0400

    add bootstrap as sass

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import bootstrap from 'bootstrap'
+import '../src/css/main.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
