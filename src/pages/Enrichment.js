310e8e5a6b6852e2f1d7b34585f104a881b02733
John N. Norman <normanj777@gmail.com>
2022-09-09 09:58:47 -0400

    refactor tests

--- src/pages/Enrichment.js
+++ src/pages/Enrichment.js
@@ -223,24 +223,27 @@ const Enrichment = () => {
               <label htmlFor="firstName" className="form-label">
                 First Name
               </label>
-              <input id="firstName" type="text" />
+              <input id="firstName" type="text" className="form-control" />
               <label htmlFor="lastName" className="form-label">
                 Last Name
               </label>
-              <input id="lastName" type="text" />
+              <input id="lastName" type="text" className="form-control" />
               <label htmlFor="email" className="form-label">
                 E-mail
               </label>
-              <input id="email" type="text" />
+              <input id="email" type="text" className="form-control" />
               <label htmlFor="phoneNumber" className="form-label">
                 Phone
               </label>
-              <input id="phoneNumber" type="number" />
+              <input id="phoneNumber" type="number" className="form-control" />
+              <p>(Optional)</p>
               <label htmlFor="tripOption" className="form-label">
                 Trip Option
               </label>
-              <input id="tripOption" type="text" />
-              <button disabled>Sign Up</button>
+              <input id="tripOption" type="text" className="form-control" />
+              <button className="btn btn-primary btn-lg my-4" disabled>
+                Sign Up
+              </button>
             </div>
           </form>
         </section>
