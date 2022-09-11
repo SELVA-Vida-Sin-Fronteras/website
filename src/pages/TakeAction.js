c0c2de84bdf955e3c9d0eb9cc189289ba67d982a
John N. Norman <normanj777@gmail.com>
2022-09-11 14:10:55 -0400

    add content

--- src/pages/TakeAction.js
+++ src/pages/TakeAction.js
@@ -1,7 +1,19 @@
 const TakeAction = () => {
   return (
-    <div data-testid="take-action">
-      <h1>Take Action</h1>
+    <div data-testid="take-action" className="page p-5">
+      <h1 className="text-center">Take Action</h1>
+      <section className="card p-4 mx-auto my-5" style={{ maxWidth: '40rem'}}>
+        <h2>Donate</h2>
+        
+        <p>
+          Become a monthly sustainer. Your donation goes directly to support the
+          work of the Amazon Reserve for Peace .
+        </p>
+        <a className="btn btn-primary btn-lg my-4" href="https://secure.squarespace.com/checkout/donate?donatePageId=5fc82b9ff97c193540e54e8a&ss_cvr=1ee9f121-670a-44f6-9577-19d3ec9a6f8e%7C1647975355020%7C1656005177842%7C1656507308687%7C17&ss_cvt=1656507308687&websiteId=5f80a2fbd5a7fe3c49230053">
+          Donate
+        </a>
+        <p>Our U.S. tax ID number is: 83-0767820</p>
+      </section>
     </div>
   );
 };
