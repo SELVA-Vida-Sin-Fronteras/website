17c17819cc09809e6fb222d925e4dadf201fa2f8
John N. Norman <normanj777@gmail.com>
2022-09-22 14:44:34 -0400

    add input fields

--- src/__tests__/SignUpForm.spec.js
+++ src/__tests__/SignUpForm.spec.js
@@ -2,10 +2,35 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SignUpForm from "../components/SignUpForm";
 
+let button;
+const setup = () => {
+  render(<SignUpForm />);
+  button = screen.queryByRole("button", { name: "Sign Up" });
+};
+
+beforeEach(() => setup());
+
 describe("Signup Form", () => {
-  render(<SignUpForm></SignUpForm>)
-  it("should have a first name input", () => {
-    const firstName = screen.getByLabelText("First Name");
-    expect(firstName).toBeInTheDocument();
+  it.each`
+    name              | inputField
+    ${"First Name"}   | ${"first name"}
+    ${"Last Name"}    | ${"last name"}
+    ${"Email"}        | ${"email"}
+    ${"Phone Number"} | ${"phone"}
+    ${"Trip Option"}  | ${"trip option"}
+  `("should have a $inputField input field", ({ name }) => {
+    const inputField = screen.getByLabelText(name);
+    expect(inputField).toBeInTheDocument();
+  });
+  it("should have the email type", ()=> {
+    const emailField = screen.getByLabelText("Email")
+    expect(emailField.type).toBe("email")
+  })
+  it("should have the type 'tel'",()=>{
+    const phoneNumber = screen.getByLabelText("Phone Number")
+    expect(phoneNumber.type).toBe("tel")
+  })
+  it("should have a submit button", () => {
+    expect(button).toBeInTheDocument();
   });
 });
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -13,6 +13,31 @@ export default function SignUpForm() {
           </label>
           <input id="firstName" className="form-control"></input>
         </Form.Field>
+        <Form.Field>
+          <label htmlFor="lastName" className="form-label">
+            Last Name
+          </label>
+          <input id="lastName" className="form-control"></input>
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input id="email" className="form-control" type="email"></input>
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="phone" className="form-label">
+            Phone Number
+          </label>
+          <input id="phone" className="form-control" type="tel"></input>
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="trip-option" className="form-label">
+            Trip Option
+          </label>
+          <input id="trip-option" className="form-control"></input>
+        </Form.Field>
+        <button className="btn btn-primary btn-lg my-4">Sign Up</button>
       </Form>
     </div>
   );
