c4b09f609d3c8def7b0565ff12eca99a99f75cc0
John N. Norman <normanj777@gmail.com>
2022-10-06 09:31:07 -0400

    fix sass config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/js/bootstrap'
+import './custom.scss'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
